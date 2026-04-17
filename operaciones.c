0755d25ae60969926b6b07f5ab1c1a2eae622eff
Jorge Julian Quartino <jorgejquartino@gmail.com>
2026-04-16 23:42:36 -0300

    Mejora: se explica el algoritmo de multiplicar

--- operaciones.c
+++ operaciones.c
@@ -14,6 +14,7 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 //Multiplicar funciona de la misma manera que sumar y restar, pero usando el operador de multiplicación (*).
+//El algoritmo de multiplicar funciona sumando 'a' veces 'b'.
 int multiplicar(int a, int b) {
     return a * b;
 }
